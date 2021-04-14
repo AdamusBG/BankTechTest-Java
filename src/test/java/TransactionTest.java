70682f5c04eaba7f80e22f83620d7a18ddee2caf
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-14 14:54:24 +0100

    added test for sorting Transactions in an ArrayList

--- src/test/java/TransactionTest.java
+++ src/test/java/TransactionTest.java
@@ -1,8 +1,9 @@
 import Entities.Transaction;
+import Utils.TransactionComparator;
 
 import org.junit.jupiter.api.Test;
-
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,6 +39,41 @@ public class TransactionTest {
 
         assertEquals(3, testTransaction.getNumberInDay());
     }
+
+    @Test
+    void severalTransactionsInArrayListCanBeSorted() {
+        // initialize ArrayList to be sorted
+        ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
+
+        // creating dummy transactions to add
+        Transaction t1 = new Transaction("01/01/2021", 10.00, 0.0, 1);
+        Transaction t2 = new Transaction("01/01/2021", 20.00, 0.0, 2);
+        Transaction t3 = new Transaction("01/01/2021", 30.00, 0.0, 3);
+        Transaction t4 = new Transaction("02/01/2021", 40.00, 0.0, 1);
+        Transaction t5 = new Transaction("02/01/2021", 50.00, 0.0, 2);
+        Transaction t6 = new Transaction("03/01/2021", 60.00, 0.0, 1);
+
+        // adding transactions in random order
+        transactionList.add(t4);
+        transactionList.add(t6);
+        transactionList.add(t2);
+        transactionList.add(t5);
+        transactionList.add(t3);
+        transactionList.add(t1);
+
+        // sorting call
+        transactionList.sort(new TransactionComparator());
+
+        // testing that sorting has worked correctly
+        assertEquals(10.00, transactionList.get(0).getMovement());
+        assertEquals(20.00, transactionList.get(1).getMovement());
+        assertEquals(30.00, transactionList.get(2).getMovement());
+        assertEquals(40.00, transactionList.get(3).getMovement());
+        assertEquals(50.00, transactionList.get(4).getMovement());
+        assertEquals(60.00, transactionList.get(5).getMovement());
+    }
+
+
 
     private void initializeTestTransaction() {
         this.testTransaction = new Transaction("14/04/2021", 123.45, 1230.50, 3);
