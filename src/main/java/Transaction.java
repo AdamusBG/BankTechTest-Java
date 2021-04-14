32021e2d5f2946ffa3368283b3d06339b2cb47db
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-14 13:32:26 +0100

    created prototype of Transaction class

---
+++ src/main/java/Transaction.java
@@ -0,0 +1,45 @@
+import java.util.Calendar;
+
+import static java.lang.Integer.parseInt;
+
+public class Transaction {
+
+    private final Calendar date;
+    private final double movement;
+    private final double balance;
+    private final int numberInDay;
+
+    public Transaction(double movement, double balance, int numberInDay) {
+        this.date = Calendar.getInstance();
+        this.movement = movement;
+        this.balance = balance;
+        this.numberInDay = numberInDay;
+    }
+
+    public Transaction(String transactionDate, double movement, double balance, int numberInDay) {
+        this(movement, balance, numberInDay);
+        setDate(transactionDate);
+    }
+
+    public Calendar getDate() {
+        return this.date;
+    }
+
+    public double getMovement() {
+        return this.movement;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public int getNumberInDay() {
+        return this.numberInDay;
+    }
+
+    private void setDate(String transactionDate) {
+        // Could check date here
+        String[] parts = transactionDate.split("/");
+        this.date.set(parseInt(parts[2]), parseInt(parts[1]) - 1, parseInt(parts[0]));
+    }
+}
