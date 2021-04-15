3bcc569a57582b5a5c194505bb1318a2784b632b
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-15 12:11:34 +0100

    updated TransactionComparator to change ordering

--- src/main/java/Utils/TransactionComparator.java
+++ src/main/java/Utils/TransactionComparator.java
@@ -8,8 +8,8 @@ public class TransactionComparator implements Comparator<Transaction> {
     @Override
     public int compare(Transaction t1, Transaction t2) {
         if (t1.getDate().equals(t2.getDate())) {
-            return t1.getNumberInDay() - t2.getNumberInDay();
+            return t2.getNumberInDay() - t1.getNumberInDay();
         }
-        return t1.getDate().compareTo(t2.getDate());
+        return t2.getDate().compareTo(t1.getDate());
     }
 }
