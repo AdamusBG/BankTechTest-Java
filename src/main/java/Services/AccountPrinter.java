050e8648609e8d6500ced90be277f25c73dbcbd9
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-16 13:33:54 +0100

    implemented AccountPrinter#printStatement

--- src/main/java/Services/AccountPrinter.java
+++ src/main/java/Services/AccountPrinter.java
@@ -1,5 +1,9 @@
 package Services;
 
+import Entities.Transaction;
+
+import java.util.ArrayList;
+
 public class AccountPrinter {
     public static void printIncorrectDateMessage(String date) {
         System.out.println(String.format("The given date of '%s' is invalid, please provide a valid date in format dd/mm/yyyy", date));
@@ -14,6 +18,20 @@ public class AccountPrinter {
     public static void printNotEnoughBalance() {
         System.out.println("There is not enough balance to process this transaction");
         printTransactionCancelled();
+    }
+
+    public static void printStatement(ArrayList<Transaction> transactions) {
+        String statementToPrint = "date || credit || debit || balance\n";
+        for (Transaction t : transactions) {
+            statementToPrint += t.getDateAsString();
+            if (t.getMovement() >= 0) {
+                statementToPrint += String.format(" || %.02f || || ", t.getMovement());
+            } else {
+                statementToPrint += String.format(" || || %.02f || ", t.getMovement() * -1);
+            }
+            statementToPrint += String.format("%.02f\n", t.getBalance());
+        }
+        System.out.print(statementToPrint);
     }
 
     private static void printTransactionCancelled() {
