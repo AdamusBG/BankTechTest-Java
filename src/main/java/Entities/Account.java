0fb790e5a27c56336f14626664b0e734e56e284c
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-16 11:33:52 +0100

    updated Account class to pass tests

--- src/main/java/Entities/Account.java
+++ src/main/java/Entities/Account.java
@@ -1,5 +1,7 @@
 package Entities;
 
+import Utils.AccountUtils;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
@@ -7,11 +9,19 @@ import java.text.SimpleDateFormat;
 public class Account {
 
     private ArrayList<Transaction> transactions;
-    private double balance;
+    private int balance;
 
     public Account() {
         this.transactions = new ArrayList<Transaction>();
-        this.balance = 0.0;
+        this.balance = 0;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return this.transactions;
+    }
+
+    public double getBalance() {
+        return (double) this.balance / 100;
     }
 
     public void withdraw(double amount) {
@@ -20,22 +30,63 @@ public class Account {
     }
 
     public void withdraw(String date, double amount) {
-        // check date format
-        // check amount positive
-        // check enough balance to make withdrawal
-        // actually reduce balance, maybe by calling private method
-        // add a transaction object to arraylist
+        if (!AccountUtils.isValidDate(date)) {
+            System.out.println(String.format("The given date of '%s' is invalid, please provide a valid date in format dd/mm/yyyy", date));
+            System.out.println("This transaction has been cancelled");
+            return;
+        }
+        if (amount < 0) {
+            System.out.println("The amount must be positive");
+            System.out.println("This transaction has been cancelled");
+            return;
+        }
+        if (!AccountUtils.enoughForWithdrawal(this.balance, amount)) {
+            System.out.println("There is not enough balance to process this transaction");
+            System.out.println("This transaction has been cancelled");
+            return;
+        }
+        changeBalance(-1 * amount);
+        recordTransaction(date, -1 * amount);
     }
 
-    public void add(double amount) {
+    public void deposit(double amount) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        add(formatter.format(Calendar.getInstance().getTime()), amount);
+        deposit(formatter.format(Calendar.getInstance().getTime()), amount);
     }
 
-    public void add(String date, double amount) {
-        // check date format
-        // check amount positive
-        // actually reduce balance, maybe by calling private method
-        // add a transaction object to arraylist
+    public void deposit(String date, double amount) {
+        if (!AccountUtils.isValidDate(date)) {
+            System.out.println(String.format("The given date of '%s' is invalid, please provide a valid date in format dd/mm/yyyy", date));
+            System.out.println("This transaction has been cancelled");
+            return;
+        }
+        if (amount < 0) {
+            System.out.println("The amount must be positive");
+            System.out.println("This transaction has been cancelled");
+            return;
+        }
+        changeBalance(amount);
+        recordTransaction(date, amount);
+    }
+
+    private void changeBalance(double amount) {
+        int minorUnitAmount = (int) (amount * 100);
+        this.balance += minorUnitAmount;
+    }
+
+    private void recordTransaction(String date, double amount) {
+        int numberInDay = transactionsOnThisDate(date) + 1;
+        Transaction toRecord = new Transaction(date, amount, getBalance(), numberInDay);
+        this.transactions.add(toRecord);
+    }
+
+    private int transactionsOnThisDate(String date) {
+        int transactionsOnDate = 0;
+        for (Transaction t : this.transactions) {
+            if (t.getDateAsString().equals(date)) {
+                transactionsOnDate += 1;
+            }
+        }
+        return transactionsOnDate;
     }
 }
