55c104ea6d96a432340e58616ea0b1bb7f9c3880
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-16 13:57:29 +0100

    updated Main to include example usage of the program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import Entities.Account;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Account myAccount = new Account();
+
+        myAccount.deposit("01/03/2021", 1000);
+        myAccount.withdraw("01/03/2021", 27.34);
+        myAccount.withdraw("02/03/2021", 30.00);
+        myAccount.deposit("03/03/2021", 150.00);
+
+        myAccount.statement();
+
+        // uncomment the below to see example of incorrect date message
+        // myAccount.withdraw("41/03/2021", 20.00);
+
+        // uncomment the below to see example of amount must be positive message
+        // myAccount.withdraw("03/03/2021" -100);
+
+        // uncomment the below to see example of not enough balance method
+        // myAccount.withdraw("03/03/2021" 5000);
     }
 }
