d1b38096b8b3427289478ca9344233a7b1ea7501
Adam Grieve <adamwizzy@hotmail.co.uk>
2021-04-16 12:02:00 +0100

    implemented printing methods in AccountPrinter class

--- src/main/java/Services/AccountPrinter.java
+++ src/main/java/Services/AccountPrinter.java
@@ -1,4 +1,22 @@
 package Services;
 
 public class AccountPrinter {
+    public static void printIncorrectDateMessage(String date) {
+        System.out.println(String.format("The given date of '%s' is invalid, please provide a valid date in format dd/mm/yyyy", date));
+        printTransactionCancelled();
+    }
+
+    public static void printAmountMustBePositive() {
+        System.out.println("The amount must be positive");
+        printTransactionCancelled();
+    }
+
+    public static void printNotEnoughBalance() {
+        System.out.println("There is not enough balance to process this transaction");
+        printTransactionCancelled();
+    }
+
+    private static void printTransactionCancelled() {
+        System.out.println("This transaction has been cancelled");
+    }
 }
